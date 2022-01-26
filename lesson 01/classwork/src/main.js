9af4feebeec0c9f10a3fbc098155a4c56b7d8d20
Oleg <oleg.lustenko@gmail.com>
2022-01-26 21:30:29 +0200

    lesson 1

---
+++ lesson 01/classwork/src/main.js
@@ -0,0 +1,50 @@
+let number = '1234';
+const newNumber = "99999";
+var something = 'privet kak dela';
+
+// console.log(newNumber);
+
+// Сделай переменную и назови ее как хочешь
+// Переменная должна содержать твое имя
+// отобрази свое имя в консоле
+const qwecqwec1c213awecqaw = 'Vya4';
+
+
+const myBoolean = true;
+const myBooleanFalse = false;
+// console.log(asd);
+
+const myNumber = 12345;
+const myNumber100 = 2;
+const myNumber200 = 3;
+
+console.log(myNumber100);
+
+const myString = '100';
+const myName = 'Oleh';
+const mySecondName = 'Lustenko';
+
+console.log(myName + ' ' + mySecondName);
+
+console.log('WWWWWWWWWWWWWWWWW', 100 / 'qwe');
+
+const myNull = null;
+const myUndefined = undefined;
+const qwe = NaN; // not a number
+
+const array = [myString, myName, myNull, myUndefined];
+console.log(array);
+
+const user = {
+  name: 'Oleh',
+  surname: 'Lustenko',
+  isTeacher: true,
+  salary: 10000,
+  car: {
+    model: 'Mitsubishi',
+    wheels: 4,
+  },
+  houses: array
+};
+
+console.log(user);
